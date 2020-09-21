00adf088b795e7bc1c67beecb0d1183c650bd80d
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-09-21 23:00:23 +0100

    Created types file

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import React from "react";
 import { TodoListItem } from "./TodoListItem";
 
-type Todo = {
-  text: string;
-  complete: boolean;
-};
-
 const todos: Array<Todo> = [
   { text: "buy coffee", complete: true },
   { text: "eat pizza", complete: false },
 ];
 
 function App() {
-  return <TodoListItem />;
+  return <TodoListItem todo={todos[0]} />;
 }
 
 export default App;
---
+++ src/types.d.ts
@@ -0,0 +1,5 @@
+//type declaration file(by adding .d in the name of the file)- no longer need to export
+type Todo = {
+  text: string;
+  complete: boolean;
+};
